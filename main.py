bf323e2b50791e80f772c77638e86aa7ab07f644
divyansh-1901 <89969262+divyansh-1901@users.noreply.github.com>
2022-12-28 13:28:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from time_calculator import add_time
+from unittest import main
+
+
+print(add_time("11:43 PM", "24:20", "tueSday"))
+
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
